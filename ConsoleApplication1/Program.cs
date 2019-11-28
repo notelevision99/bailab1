edff895209a47b5b6a38a4be7cd86ff8c0499b0c
Administrator <Administrator@PC275>
2019-11-28 13:51:48 +0700

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap tong so sinh vien N:");
+            int n = int.Parse(Console.ReadLine());
+            Student[] stu = new Student[n];
+            Console.WriteLine("\n==========Nhap DS Sinh Vien============");
+            for(int i=0;i<n;i++)
+            {
+                stu[i] = new Student();
+                Console.WriteLine("Nhap Ma SV: " + i + 1);
+                stu[i].StudentID = Console.ReadLine();
+                Console.WriteLine("Nhap Ten: ");
+                stu[i].FullName = Console.ReadLine();
+                Console.WriteLine("Nhap DTB: ");
+                stu[i].AverageScore = float.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap Khoa: ");
+                stu[i].Faculty = Console.ReadLine();
+            }
+            Console.WriteLine("=========Xuat DS Sinh Vien==============");
+            foreach(Student sv in stu)
+            {
+                Console.WriteLine("MSSV: " + sv.StudentID);
+                Console.WriteLine("Name: " + sv.FullName);
+                Console.WriteLine("DTB: " + sv.AverageScore);
+                Console.WriteLine("Khoa: " + sv.Faculty);
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+        private string studentID;
+        private string fullName;
+        private float averageScore;
+        private string faculty;
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+        public float AverageScore { get; set; }
+        public string Faculty { get; set; }
+        public Student()
+        {
+
+        }
+        public Student(string id,string name,float score,string falculty)
+        {
+            studentID = id;
+            fullName = name;
+            averageScore = score;
+            faculty = falculty;
+        }
+    }
+}
